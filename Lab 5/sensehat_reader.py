1b930e17186ba09bb185b6fef193e89b0e1cccd6
Musham Varun <mushamvarun130@gmail.com>
2025-03-28 09:04:53 +0530

    Delete Lab 5/sensehat_reader.py

--- Lab 5/sensehat_reader.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3 
-from sense_emu import SenseHat 
-sense = SenseHat() 
-temp = sense.get_temperature() 
-humidity = sense.get_humidity() 
-print(f"{temp:.2f},{humidity:.2f}")  # Outputs "temperature,humidity"
